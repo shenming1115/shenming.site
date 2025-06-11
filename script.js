e001dbe2d96f373338f86b530e48709d7576231a
Ch'ng Shen Ming <shenming0387@gmail.com>
2025-06-11 17:56:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function sayHi() {
-    console.log("Hello, World!");
-    alert("Hello, World!");
-    document.getElementById("greeting").innerText = "Hello, World!";
-}
+// 当页面加载时，弹出警告框
+window.onload = function () {
+  alert('Welcome to Shenming Site!');
+};
+
+// 播放音频并增加音量
+document.getElementById('playButton').onclick = function () {
+  const audioPlayer = document.getElementById('audioPlayer');
+  audioPlayer.volume = 1.0;  // 设置音量为最大（1.0）
+  audioPlayer.play();
+};
